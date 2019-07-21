762e75fc027fcc7d74cf07c648d7c63a58f8acee
AsemDevs <asemhamdi97@gmail.com>
2019-07-21 21:55:55 +0100

    connect db and test queries

--- log.py
+++ log.py
@@ -2,4 +2,8 @@ import psycopg2
 
 db = psycopg2.connect("dbname=news")
 c = db.cursor()
-c.execute("")
+query = "SELECT title FROM articles LIMIT 5;"
+c.execute(query)
+results = c.fetchall()
+print(results)
+db.close()
