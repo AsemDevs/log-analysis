6198423fd18f7de7ba0c0a99119ca2af882813ee
AsemDevs <asemhamdi97@gmail.com>
2019-07-23 00:58:04 +0100

    Edit query1 for more clear code

--- log.py
+++ log.py
@@ -1,20 +1,23 @@
 import psycopg2
 
+
+question1 = '1. What are the most popular three articles of all time?'
+
+query1 = '''SELECT SUBSTRING(path,10) AS slug,
+ COUNT(*) as views FROM log
+ WHERE path LIKE '%-%'
+ GROUP BY path ORDER BY views DESC LIMIT 3;'''
+
 def QueryExecute(querys):
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute(querys)
     results = c.fetchall()
-    print(results)
     db.close()
+    print(results)
 
-query1 = '''SELECT path, COUNT(*) as views FROM log WHERE path IN ('/article/bad-things-gone',
-    '/article/balloon-goons-doomed'
-    ,'/article/bears-love-berries'
-    ,'/article/candidate-is-jerk'
-    ,'/article/goats-eat-googles'
-    ,'/article/media-obsessed-with-bears'
-    ,'/article/trouble-for-troubled'
-    ,'/article/so-many-bears') GROUP BY path ORDER BY views DESC;'''
 
+print(question1)
 QueryExecute(query1)
+
+
