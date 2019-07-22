9d4d9c7941e80298352855f8c0cdae9eb1960025
AsemDevs <asemhamdi97@gmail.com>
2019-07-22 04:29:06 +0100

    Add the first query for the first question

--- log.py
+++ log.py
@@ -1,9 +1,20 @@
 import psycopg2
 
-db = psycopg2.connect("dbname=news")
-c = db.cursor()
-query = "SELECT title FROM articles LIMIT 5;"
-c.execute(query)
-results = c.fetchall()
-print(results)
-db.close()
+def QueryExecute(querys):
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute(querys)
+    results = c.fetchall()
+    print(results)
+    db.close()
+
+query1 = '''SELECT path, COUNT(*) as views FROM log WHERE path IN ('/article/bad-things-gone',
+    '/article/balloon-goons-doomed'
+    ,'/article/bears-love-berries'
+    ,'/article/candidate-is-jerk'
+    ,'/article/goats-eat-googles'
+    ,'/article/media-obsessed-with-bears'
+    ,'/article/trouble-for-troubled'
+    ,'/article/so-many-bears') GROUP BY path ORDER BY views DESC;'''
+
+QueryExecute(query1)
