ad8400426ddcc400cc10db0872fcb94cc2d9de32
AsemDevs <asemhamdi97@gmail.com>
2019-07-25 22:36:09 +0100

    Applying pycodestyle

--- log.py
+++ log.py
@@ -8,24 +8,33 @@ question1 = '1. What are the most popular three articles of all time?'
 query1 = '''SELECT * FROM num_views LIMIT 3;'''
 
 question2 = '2. Who are the most popular article authors of all time?'
-query2 = '''SELECT name, SUM(views) FROM authors_data GROUP BY name, author ORDER BY author;'''
+query2 = '''SELECT name, SUM(views) FROM authors_data
+ GROUP BY name, author ORDER BY author;'''
 
 question3 = "3. On which days did more than 1% of requests lead to errors?"
-query3 = '''SELECT date, CAST(percent AS numeric) FROM ratio WHERE percent > 1;'''
-def results(querys):
+query3 = '''SELECT date, CAST(percent AS numeric)
+ FROM ratio WHERE percent > 1;'''
+
+
+# Connect to database and execute queries
+def results(queries):
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
-    c.execute(querys)
+    c.execute(queries)
     results = c.fetchall()
     db.close()
     return results
 
-r1=results(query1)
-r2=results(query2)
-r3=results(query3)
 
+# Get the results of each query
+r1 = results(query1)
+r2 = results(query2)
+r3 = results(query3)
+
+
+# Output function To unpack the list of the tuples
+# and get only plain text for the first two queries
 def report_results(q):
-    # out = list(zip(*q))
     out = list(itertools.chain(*q))
     for i in range(len(out)):
         if i % 2 == 0:
@@ -35,6 +44,9 @@ def report_results(q):
             print("\t" + "%s - %d" % (slug, num) + " views")
             print("\n")
 
+
+# Output function To unpack the list of the tuples
+# and get only plain text for the third query and specific edits
 def report_results_q3(q):
     out = list(itertools.chain(*q))
     for i in range(len(out)):
@@ -42,10 +54,11 @@ def report_results_q3(q):
             slug = out[i]
         else:
             num = out[i]
-            x = format(num/100,'%')
-            print("\t" + slug +" - "+ x + " errors")
+            x = format(num/100, '%')
+            print("\t" + slug + " - " + x + " errors")
 
 
+# printing the questions and calling output functions
 print(question1)
 report_results(r1)
 print(question2)
